ae256260a5420ef16c8a64afa752c81ea2225ac4
Aleksey Viktorovich Povarov <5656738@mail.ru>
2019-07-12 14:36:26 +0300

    Adding form for url

--- index.php
+++ index.php
@@ -9,4 +9,16 @@ if (!empty($strLink)){
     }else{
         echo('Не удалось выполнить переадресацию на сайт '.$strLink);
     }
+}else{
+?>
+<form method="get">
+    <label for="url">
+        Enter URL to redirect:
+    </label>
+    &nbsp;&nbsp;
+    <input id="url" type="text" name="url" placeholder="any URL"/>
+    &nbsp;&nbsp;
+    <button type="submit">Find site and redirect</button>
+</form>
+<?
 }
